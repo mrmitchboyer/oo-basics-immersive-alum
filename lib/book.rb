a4e621c6c803b56dee0fb0ed3da8de617de51410
github username <mitch@mitchboyer.com>
2017-11-20 20:13:36 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_reader :title
+  attr_accessor :genre, :author, :page_count
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
